3016bf1676de60f7eae862e49281b2cfe541ff3c
ChuYong <jakgon@naver.com>
2024-01-03 12:15:22 +0900

    feat: test seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import {ArticleJsonLd, DefaultSeo} from "next-seo";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="kr">
+      <head>
+        <ArticleJsonLd
+          useAppDir={true}
+          url="https://no5ing.kr"
+          title="삐삐 - 가족 초대 링크가 도착했어요"
+          authorName={"bbibbi"}
+          datePublished={
+            "2023-01-01T00:00:00+09:00"
+          }
+          description={"입장하고 일상공유를 시작해보세요"}
+          images={
+            [
+              "https://no5ing.kr/og_image.png"
+            ]
+          }
+        />
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
