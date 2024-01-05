aaa513598d758316fbd40578fd81635043451e01
송영민 <jakgon@naver.com>
2024-01-05 19:21:45 +0900

    feat: update deep link page

--- app/o/[id]/page.tsx
+++ app/o/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import {useEffect, useState} from "react";
+import Image from "next/image";
 
 export default function Page() {
   const appStoreUrl = 'https://itunes.apple.com/kr/app/id393499958';
   const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.no5ing.bbibbi';
+  const appStoreInfo = (<span>AppStore로 이동중..<br/><a href={appStoreUrl}>직접 이동하기</a></span>);
+  const playStoreInfo = (<span>PlayStore로 이동중..<br/><a href={playStoreUrl}>직접 이동하기</a></span>);
   const [platform, setPlatform] = useState<"unknown" | "ios" | "android">("unknown");
   useEffect(() => {
     const detectPlatform = () => {
@@ -25,8 +28,13 @@ export default function Page() {
         location.href = playStoreUrl;
       }, 50);
     }
+
   }, []);
-  return <>
-    {platform == "unknown" ? <span>모바일에서만 접근할 수 있습니다.</span> : <span>AppStore로 이동중..<br/><a href={appStoreUrl}>직접 이동하기</a></span>}
-  </>;
+  return <div className={"flex flex-col justify-center items-center h-screen w-screen gap-8"}>
+    <Image src="/oing_icon.png" width={200} height={200} />
+    <div className={"text-center text-lg text-gray-300"}>
+      {platform == "unknown" ? <span>모바일에서만 접근할 수 있어요</span> : (platform == "ios" ? appStoreInfo : playStoreInfo)}
+    </div>
+
+  </div>;
 }
