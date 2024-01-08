ef6210549275f4d31ae28aedb233ecdb0b6b3772
ChuYong <jakgon@naver.com>
2024-01-08 10:56:25 +0900

    feat: add notion redirect

--- app/app/contact/page.tsx
+++ app/app/contact/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <div>개발자</div>
+'use client'
+import {useEffect} from "react";
+
+export default function Terms() {
+  useEffect(() => {
+    location.href = 'https://emerald-rosemary-a94.notion.site/Bibbi-c3dd457b32564f369c1a08c42c0ab00e';
+  }, []);
+  return <></>
 }
