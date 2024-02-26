2546a5539e79161c3f5db179e349e1287f2835dd
송영민 <jakgon@naver.com>
2024-02-26 17:28:39 +0900

    feat: little bit PC compatibility

--- app/(website)/main.tsx
+++ app/(website)/main.tsx
@@ -17,10 +17,13 @@ import Image from "next/image";
 import Link from "next/link";
 import {detectPlatform} from "@/src/util";
 import NavigationBar from "@/components/layout/NavigationBar";
+import useWindowDimensions from "@/components/WindowDimensions";
+import { useViewPortDetector } from "@/components/WindowDimensions";
 
 
 export default function Main() {
     const [scrollY, setScrollY] = useState(0);
+    const isMobile = useViewPortDetector();
     const [appDownloadUrl, setAppDownloadUrl] = useState("/");
     const onScroll = useCallback(() => {
         const {pageYOffset} = window;
@@ -41,6 +44,10 @@ export default function Main() {
             window.removeEventListener("scroll", onScroll);
         }
     }, []);
+    useEffect(() => {
+        console.log("isMobile", isMobile)
+    }, [isMobile]);
+
     const [isNavBarFull, setIsNavBarFull] = useState(false);
 
     useEffect(() => {
@@ -50,11 +57,11 @@ export default function Main() {
         <header className={"fixed z-40 top-0"}>
             <NavigationBar isFull={isNavBarFull}/>
         </header>
-        <div className={"flex flex-col justify-start items-start"}>
-            <FirstSlide appDownloadUrl={appDownloadUrl}/>
-            <SecondSlide/>
-            <ThirdSlide/>
-            <FourthSlide/>
+        <div className={"flex flex-col justify-start items-start w-full"}>
+            <FirstSlide appDownloadUrl={appDownloadUrl} isMobile={isMobile}/>
+            <SecondSlide isMobile={isMobile}/>
+            <ThirdSlide isMobile={isMobile}/>
+            <FourthSlide isMobile={isMobile}/>
             <FifthSlide/>
             <SixthSlide/>
             <Las2tSlide/>
@@ -64,7 +71,7 @@ export default function Main() {
     </>
 }
 
-function FirstSlide({appDownloadUrl}: { appDownloadUrl: string }) {
+function FirstSlide({appDownloadUrl, isMobile}: { appDownloadUrl: string, isMobile: boolean }) {
     return (<div className={"flex flex-col justify-between items-center h-dvh w-screen bg-yellow-100 text-black pt-16 "}
                  style={{
                      background: "url('/bibbi_repeatable_background.svg')",
@@ -82,15 +89,18 @@ function FirstSlide({appDownloadUrl}: { appDownloadUrl: string }) {
                 animate={{opacity: 1,  transform: "translate3d(0, 0, 0)"}}
             >
                 <div className={"flex flex-col justify-center items-center"}>
-                    <p className={"text-center font-extrabold text-4xl leading-snug text-bsdg932wf"}>
+                    <p className={
+                        isMobile ? "text-center font-extrabold text-4xl leading-snug text-bsdg932wf":
+                            "text-center font-extrabold text-5xl leading-snug text-bsdg932wf"}>
                         하루 한번, <br/>
                         가족에게 보내는<br/>
                         생존신고, 삐삐
                     </p>
-                    <div className={"h-4"}/>
+                    <div className={isMobile? "h-4" : "h-8"}/>
                     <Link href={appDownloadUrl}>
-                        <div className={"rounded-3xl bg-a39d px-6 py-4 w-fit"}>
-                            <div className={"text-base text-white text-center leading-none font-bold"}>
+                        <div className={isMobile ? "rounded-3xl bg-a39d px-6 py-4 w-fit" : "rounded-3xl bg-a39d px-8 py-6 w-fit"}>
+                            <div className={isMobile ? "text-base text-white text-center leading-none font-bold" :
+                                "text-2xl text-white text-center leading-none font-bold"}>
                                 <span>앱 다운로드</span>
                             </div>
                         </div>
@@ -107,14 +117,15 @@ function FirstSlide({appDownloadUrl}: { appDownloadUrl: string }) {
 }
 
 
-function SecondSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white py-16 px-8 bg-19191a"}>
+function SecondSlide({isMobile}: { isMobile: boolean}) {
+    return (<>
         <motion.div
             key={"second-slide-a"}
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
-            className={"w-full"}
+            className={isMobile ? "flex flex-col justify-between items-start w-screen text-white py-16 px-8 bg-19191a"
+                : "flex flex-row justify-between items-center text-white py-20 px-16 bg-19191a max-w-screen-xl mx-auto w-full pt-48"}
             transition={{
                 delay: 0.5,
                 duration: 1.0
@@ -130,36 +141,40 @@ function SecondSlide() {
                 }
             }}
         >
-            <div className={"font-extrabold text-2xl leading-snug"}>
-                <p>
-                    <span className={"text-f5f378"}>가족의 하루</span>를
-                </p>
-                <p>
-                    알고 계시나요?
-                </p>
+            <div className={"flex flex-col"}>
+                <div className={isMobile ? "font-extrabold text-2xl leading-snug" : "font-extrabold text-4xl leading-snug"}>
+                    <p>
+                        <span className={"text-f5f378"}>가족의 하루</span>를
+                    </p>
+                    <p>
+                        알고 계시나요?
+                    </p>
+                </div>
+                <div className={"h-4"}/>
+                <div className={isMobile ? "text-b2b2b4 text-base" : "text-b2b2b4 text-2xl"}>
+                    <p>친구와는 SNS로</p>
+                    <p>서로의 일상을 알고있지만,</p>
+                    <p>가족과는 그러기 쉽지 않죠</p>
+                </div>
+                <div className={isMobile? "h-8": ""}/>
             </div>
-            <div className={"h-4"}/>
-            <div className={"text-b2b2b4 text-base"}>
-                <p>친구와는 SNS로</p>
-                <p>서로의 일상을 알고있지만,</p>
-                <p>가족과는 그러기 쉽지 않죠</p>
-            </div>
-            <div className={"h-8"}/>
-            <Image className={"w-full"} src={LandingFirstBibbi} alt={"bibbi_hi"}/>
+
+            <Image className={isMobile ? "w-full" : "w-96"} src={LandingFirstBibbi} alt={"bibbi_hi"}/>
         </motion.div>
 
 
-    </div>)
+    </>)
 }
 
-function ThirdSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white py-16 px-8 bg-19191a"}>
+function ThirdSlide({isMobile}: { isMobile: boolean}) {
+    return (<>
         <motion.div
             key={"second-slide-a"}
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
-            className={"w-full"}
+            className={isMobile ? "flex flex-col justify-between items-start w-screen text-white py-16 px-8 bg-19191a"
+                : "flex flex-row-reverse justify-between items-center w-screen text-white py-24 px-16 bg-19191a max-w-screen-xl mx-auto w-full pb-48"}
             transition={{
                 delay: 0.5,
                 duration: 1.0
@@ -175,7 +190,7 @@ function ThirdSlide() {
                 }
             }}
         >
-            <div className={"font-extrabold text-2xl leading-snug"}>
+            <div className={isMobile ? "font-extrabold text-2xl leading-snug" : "font-extrabold text-4xl leading-snug"}>
                 <p>
                     <span className={"text-ff99b1"}>너무 잦은 연락</span>은
                 </p>
@@ -186,48 +201,53 @@ function ThirdSlide() {
                     서로 안하게 되구요
                 </p>
             </div>
-            <div className={"h-20"}/>
-            <Image className={"w-full"} src={LandingSecondBibbi} alt={"bibbi_hi"}/>
+            <div className={isMobile? "h-20": ""}/>
+            <Image className={isMobile ? "w-full" : "w-96"} src={LandingSecondBibbi} alt={"bibbi_hi"}/>
         </motion.div>
 
-    </div>)
+    </>)
 }
 
-function FourthSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-f5f378"}>
-        <div className={"font-extrabold text-2xl text-black leading-snug pt-16 px-8"}>
-            <p>
-                하루 딱 한번의
-            </p>
-            <p>
-                생존신고라면 어떨까요?
-            </p>
+function FourthSlide({isMobile}: { isMobile: boolean}) {
+    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-f5f378  w-full"}>
+        <div className={"max-w-screen-xl mx-auto w-full"}>
+            <div className={isMobile ? "font-extrabold text-2xl text-black leading-snug pt-16 px-8 ":
+                "font-extrabold text-3xl text-black leading-snug pt-16 px-8 "}>
+                <p>
+                    하루 딱 한번의
+                </p>
+                <p>
+                    생존신고라면 어떨까요?
+                </p>
+            </div>
+            <Image className={"w-full"} src={BibbiReportSample} alt={"bibbi_hi"}/>
         </div>
-        <Image className={"w-full"} src={BibbiReportSample} alt={"bibbi_hi"}/>
+
     </div>)
 }
 
 function FifthSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-a39d"}>
+    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-a39d w-full"}>
+        <div className={"max-w-screen-xl mx-auto w-full"}>
+            <div className={"font-extrabold text-2xl leading-snug py-16 px-8"}>
+                <p>
+                    단순히 공유하고 싶은
+                </p>
+                <p>
+                    일상을 찍고,
+                </p>
+                <p>
+                    업로드만 하면 끝이에요.
+                </p>
+            </div>
 
-        <div className={"font-extrabold text-2xl leading-snug py-16 px-8"}>
-            <p>
-                단순히 공유하고 싶은
-            </p>
-            <p>
-                일상을 찍고,
-            </p>
-            <p>
-                업로드만 하면 끝이에요.
-            </p>
+            <Image className={"w-full"} src={BibbiShareSample} alt={"bibbi_hi"}/>
         </div>
-
-        <Image className={"w-full"} src={BibbiShareSample} alt={"bibbi_hi"}/>
     </div>)
 }
 
 function SixthSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-19191a"} style={{
+    return (<div className={"flex flex-col justify-between items-start text-white bg-19191a w-full"} style={{
         background: "url('/bibbi_matrix.svg')",
         backgroundSize: "auto 100%",
         backgroundPosition: "cover",
@@ -239,7 +259,7 @@ function SixthSlide() {
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
-            className={"w-full"}
+            className={"max-w-screen-xl mx-auto w-full"}
             transition={{
                 delay: 0.5,
                 duration: 1.0
@@ -273,7 +293,7 @@ function SixthSlide() {
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
-            className={"w-full"}
+            className={"max-w-screen-xl mx-auto w-full"}
             transition={{
                 delay: 0.5,
                 duration: 1.0
@@ -307,7 +327,7 @@ function SixthSlide() {
             initial="hidden"
             whileInView="visible"
             viewport={{once: true}}
-            className={"w-full"}
+            className={"max-w-screen-xl mx-auto w-full"}
             transition={{
                 delay: 0.5,
                 duration: 1.0
@@ -339,16 +359,18 @@ function SixthSlide() {
 }
 
 function Las2tSlide() {
-    return (<div className={"flex flex-col justify-between items-start w-screen text-white  bg-f5f378"}>
-        <div className={"font-extrabold text-3xl leading-snug text-black pt-16 px-8"}>
-            <p>
-                더이상 친구들에게만
-            </p>
-            <p>
-                공유하지 마세요!
-            </p>
+    return (<div className={"flex flex-col justify-between items-start w-screen text-white bg-f5f378"}>
+        <div className={"max-w-screen-xl mx-auto w-full"}>
+            <div className={"font-extrabold text-3xl leading-snug text-black pt-16 px-8"}>
+                <p>
+                    더이상 친구들에게만
+                </p>
+                <p>
+                    공유하지 마세요!
+                </p>
+            </div>
+            <Image className={"w-full"} src={ShiningBibbi} alt={"bibbi_hi"}/>
         </div>
-        <Image className={"w-full"} src={ShiningBibbi} alt={"bibbi_hi"}/>
     </div>)
 }
 
---
+++ components/WindowDimensions.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height
+    };
+}
+
+export default function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState({width: 0, height: 0});
+
+    useEffect(() => {
+        setWindowDimensions(getWindowDimensions());
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
+export function useViewPortDetector() {
+    const { _, width } = useWindowDimensions();
+    const [isMobile, setIsMobile] = useState(width < 650);
+    useEffect(() => {
+        setIsMobile(width < 650);
+    }, [width]);
+    return isMobile;
+}
