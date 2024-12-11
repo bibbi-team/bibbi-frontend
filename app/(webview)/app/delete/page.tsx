2f5564d734c3b3693b66ab54f2a2e3b5665060ac
ChuYong <jakgon@naver.com>
2024-12-11 22:35:41 +0900

    feat: add delete route

---
+++ app/(webview)/app/delete/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import {useEffect} from "react";
+
+export default function Terms() {
+  useEffect(() => {
+    location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSdmnT2IdLPENsCknao0Qtfpa3K8b5wPoHeUMOhymbPZIM78LQ/viewform?usp=header';
+  }, []);
+  return <></>
+}
